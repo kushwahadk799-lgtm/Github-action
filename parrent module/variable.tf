51112f379d156e7dfad66f3c11002420656b0de0
Dhirendra Kumar <kushwahadk799@gmail.com>
2026-08-27 02:26:48 +0530

    commit pipeline

--- parrent module/variable.tf
+++ parrent module/variable.tf
@@ -1,3 +1,20 @@
-variable "rgs" {}
-variable "subnet" {}
-variable "vnet" {}
+variable "rgs" {
+  type = map(object({
+    name     = string
+    location = string
+  }))
+}
+variable "vnet" {
+  type = map(object({
+    name          = string
+    location      = string
+    address_space = list(string)
+  }))
+}
+variable "subnet" {
+  type = map(object({
+    name             = string
+    virtual_network  = string
+    address_prefixes = list(string)
+  }))
+}
