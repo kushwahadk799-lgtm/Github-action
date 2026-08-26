e35c5de62fbfc221135306e2f263466c8c87d801
Dhirendra kushwaha <kushwahadk799@gmail.com>
2026-08-27 03:50:35 +0530

    feature1 (#3)
    * ffffffffffffffffff
    
    * oyes
    
    * adding the yml file
    
    * commit pipeline
    
    * heavy correction
    
    * heavy correction
    
    * oyes yes
    
    ---------
    
    Co-authored-by: Rahul Bhatt <158439362+rahulbhatt773@users.noreply.github.com>

--- parrent module/main.tf
+++ parrent module/main.tf
@@ -8,9 +8,3 @@ module "vnet" {
   source     = "../child_module/azurerm_virtual_network"
   vnet       = var.vnet
 }
-
-module "subnet" {
-  depends_on = [module.vnet]
-  source     = "../child_module/azurerm_subnet"
-  subnet     = var.subnet
-}
--- parrent module/variable.tf
+++ parrent module/variable.tf
@@ -1,3 +1,14 @@
-variable "rgs" {}
-variable "subnet" {}
-variable "vnet" {}
+variable "rgs" {
+  type = map(object({
+    name     = string
+    location = string
+  }))
+}
+variable "vnet" {
+  type = map(object({
+    name          = string
+    rgs           = string
+    location      = string
+    address_space = list(string)
+  }))
+}
