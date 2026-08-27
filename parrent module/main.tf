4852ae9bd7ada2721e42eff548483186acb9cb04
Dhirendra Kumar <kushwahadk799@gmail.com>
2026-08-27 17:17:58 +0530

    abc1

--- parrent module/main.tf
+++
@@ -1,4 +0,0 @@
-module "rg" {
-  source = "../child_module/azurerm_resource_group"
-  rgs    = var.rgs
-}
--- parrent module/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "rgs" {
-  type = map(object({
-    name     = string
-    location = string
-  }))
-}
-
